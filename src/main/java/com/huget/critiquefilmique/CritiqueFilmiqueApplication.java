9d4299db2dda0a21c85a32e548c2629a7f9bc274
magdalenahuget <magdalena.huget@gmail.com>
2023-10-21 21:48:46 +0200

    Initial commit

---
+++ src/main/java/com/huget/critiquefilmique/CritiqueFilmiqueApplication.java
@@ -0,0 +1,13 @@
+package com.huget.critiquefilmique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritiqueFilmiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CritiqueFilmiqueApplication.class, args);
+	}
+
+}
