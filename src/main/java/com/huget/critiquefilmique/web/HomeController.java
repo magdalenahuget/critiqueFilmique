f487a2bdc9273f12fcedf81e434f6ceeba5ec662
magdalenahuget <magdalena.huget@gmail.com>
2023-10-22 12:17:28 +0200

    Add Home Controller with implementation regardin promoted movies

---
+++ src/main/java/com/huget/critiquefilmique/web/HomeController.java
@@ -0,0 +1,29 @@
+package com.huget.critiquefilmique.web;
+
+import com.huget.critiquefilmique.domain.movie.MovieDtoMapper;
+import com.huget.critiquefilmique.domain.movie.MovieService;
+import com.huget.critiquefilmique.domain.movie.dto.MovieDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private final MovieService movieService;
+
+    public HomeController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<MovieDto> promotedMovies = movieService.findAllPromotedMovies();
+        model.addAttribute("heading", "Promoted movies");
+        model.addAttribute("description", "Movies recommended by critique filmique team");
+        model.addAttribute("movies", promotedMovies);
+        return "movie-listing";
+    }
+}
