5615e02b1c262d08466eed6ee70066f5cf7a0351
magdalenahuget <magdalena.huget@gmail.com>
2023-10-22 16:48:13 +0200

    Add error 404 management

---
+++ src/main/java/com/huget/critiquefilmique/web/MovieController.java
@@ -0,0 +1,27 @@
+package com.huget.critiquefilmique.web;
+
+import com.huget.critiquefilmique.domain.movie.MovieService;
+import com.huget.critiquefilmique.domain.movie.dto.MovieDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Controller
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/film/{id}")
+    public String getMovie(@PathVariable long id, Model model) {
+        MovieDto movie = movieService.findMovieById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("movie", movie);
+        return "movie";
+    }
+}
