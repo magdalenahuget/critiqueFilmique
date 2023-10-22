f2327bc8c676d55a73f3c80b017db9d0ee723d18
magdalenahuget <magdalena.huget@gmail.com>
2023-10-22 12:02:52 +0200

    Add MovieRepository implementation with method findAllByPromotedIsTrue()

---
+++ src/main/java/com/huget/critiquefilmique/domain/movie/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.huget.critiquefilmique.domain.movie;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MovieRepository extends CrudRepository<Movie, Long> {
+    List<Movie> findAllByPromotedIsTrue();
+}
